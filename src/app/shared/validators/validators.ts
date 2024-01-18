e8a0ee03b82f8b9e2ce3bb7eae50ef5ed7d603c7
Emiliano Acevedo <81443527+darthenis@users.noreply.github.com>
2024-01-18 17:15:35 -0300

    Revert "Ev01 05"

--- src/app/shared/validators/validators.ts
+++
@@ -1,3 +0,0 @@
-
-export const regexPassword = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" // Minimum eight characters, at least one letter and one number
-export const regexEmail = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/'
