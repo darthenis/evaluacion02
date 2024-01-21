bba70127937cf4e1adf7252c04e157ed956e229a
darthenis <emi.acevedo.letras@gmail.com>
2024-01-21 12:39:36 -0300

    added form group logic to login

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from 'src/app/shared/services/validators.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  public loginGroup = this.fb.group({
+    email: ['', Validators.required ],
+    password: ['', Validators.required ]
+  })
+
+  constructor(  private fb : FormBuilder,
+                private validatorService: ValidatorsService ){}
+
+  isValidField(field : string):boolean | null{
+    return this.validatorService.isValidField(this.loginGroup, field);
+  }
+
+  
+  onHandleForm():void{
+    this.loginGroup.markAllAsTouched();
+    if(this.loginGroup.invalid) return;
+    // TODO: add service for sign up
+    console.log(this.loginGroup.value)
+  }
+
 
 }
